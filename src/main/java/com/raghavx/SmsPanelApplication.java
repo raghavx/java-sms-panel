c24bfe3a29a4383b9e7416dd7bc74bb6eef385d8
Raghvendra Sahu <rx.sahu@gmail.com>
2017-03-23 22:58:07 +0530

    project inception

---
+++ src/main/java/com/raghavx/SmsPanelApplication.java
@@ -0,0 +1,12 @@
+package com.raghavx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsPanelApplication.class, args);
+	}
+}
